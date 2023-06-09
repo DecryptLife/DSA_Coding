4a37f2d0e916dede611c0d5a9d95c42f83f54449
DecryptLife <benson.thomas2602@gmail.com>
2023-06-09 16:05:09 -0500

    Modified skip word

--- Recursion/src/skipWord.java
+++ Recursion/src/skipWord.java
@@ -1,23 +1,36 @@
 public class skipWord {
 
+//    static String skipWord(String s, String skip)
+//    {
+//        return helper(s, skip, 0);
+//
+//    }
+//
+//    static String helper(String s, String skip, int index)
+//    {
+//
+//        if((index + skip.length()) > s.length())
+//            return s;
+//
+//
+//        if(s.substring(index,index+skip.length()).equals(skip))
+//            s = s.substring(0,index) + s.substring(index+skip.length());
+//
+//
+//        return helper(s, skip, index+1);
+//    }
+
     static String skipWord(String s, String skip)
     {
-        return helper(s, skip, 0);
+        if(s.isEmpty())
+            return "";
 
-    }
-
-    static String helper(String s, String skip, int index)
-    {
-
-        if((index + skip.length()) > s.length())
-            return s;
+        if(s.startsWith(skip))
+            return skipWord(s.substring(skip.length()),skip);
+        else
+            return s.charAt(0) + skipWord(s.substring(1),skip);
 
 
-        if(s.substring(index,index+skip.length()).equals(skip))
-            s = s.substring(0,index) + s.substring(index+skip.length());
-
-
-        return helper(s, skip, index+1);
     }
     public static void main(String[] args) {
         String s = "something something something";
