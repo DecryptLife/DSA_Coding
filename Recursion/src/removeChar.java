923d82648d6ff87eaf97b5738f7c4775d82b2a9d
DecryptLife <benson.thomas2602@gmail.com>
2023-06-09 15:49:40 -0500

    Modifying the program

--- Recursion/src/removeChar.java
+++ Recursion/src/removeChar.java
@@ -3,23 +3,23 @@ public class removeChar {
     static String remove(String str)
     {
         return helper(str,0);
+
     }
 
     static String helper(String str, int index)
     {
-        if(index>str.length())
+        if(index>=str.length())
             return str;
 
         if(str.charAt(index) ==  'a')
-            str = str.substring(0,index) + str.substring(index+1,str.length());
+            str = str.substring(0,index) + str.substring(index+1);
 
         return helper(str,index+1);
     }
 
 
     public static void main(String[] args) {
-        String s = "banana";
-
+        String s = "baccad";
         System.out.println(s + " after removal: "+remove(s));
     }
 }
