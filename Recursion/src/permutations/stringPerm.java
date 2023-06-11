f655032e63049dd67a9d4d22155ba5ef8732e80b
DecryptLife <benson.thomas2602@gmail.com>
2023-06-10 23:53:59 -0500

    Returning in ArrayList

--- Recursion/src/permutations/stringPerm.java
+++ Recursion/src/permutations/stringPerm.java
@@ -4,22 +4,40 @@ import java.util.ArrayList;
 
 public class stringPerm {
 
-    static void permutations(String p, String up)
+//    static void permutations(String p, String up)
+//    {
+//        if(up.length() == 0)
+//        {
+//            System.out.println(p);
+//            return;
+//        }
+//
+//        for(int i=0;i<p.length()+1;i++)
+//            permutations(p.substring(0,i)+up.charAt(0)+p.substring(i), up.substring(1));
+//
+//
+//    }
+
+    static ArrayList<String> permutations(String p, String up)
     {
+        ArrayList<String> outer = new ArrayList<>();
+
         if(up.length() == 0)
         {
-            System.out.println(p);
-            return;
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
         }
 
         for(int i=0;i<p.length()+1;i++)
-            permutations(p.substring(0,i)+up.charAt(0)+p.substring(i), up.substring(1));
+            outer.addAll(permutations(p.substring(0,i)+up.charAt(0)+p.substring(i), up.substring(1)));
 
+        return outer;
 
     }
 
     public static void main(String[] args) {
         String s = "abc";
-        permutations("",s);
+        System.out.println(permutations("",s));
     }
 }
