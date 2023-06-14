64c04abfee3b374da99416915166064e96727980
DecryptLife <benson.thomas2602@gmail.com>
2023-06-13 23:26:02 -0500

    Including Diagonal Path

--- Backtracking/src/Maze.java
+++ Backtracking/src/Maze.java
@@ -11,9 +11,11 @@ public class Maze {
 
         int right = 0;
         int down = 0;
+        int diagonal = 0;
         if(i<row && j< column)
         {
             right += reachTarget(row,column,i,j+1);
+            diagonal += reachTarget(row,column,i+1,j+1);
             down += reachTarget(row,column,i+1,j);
         }
         else if(i==row && j<column)
@@ -25,13 +27,42 @@ public class Maze {
             right += reachTarget(row,column,i+1,j);
         }
 
-        return right + down;
+        return right + down +diagonal;
+
+
+    }
+    public static void  path(String path,int row, int column, int i, int j)
+    {
+        if(row == i && column ==j)
+        {
+            System.out.println(path);
+            return;
+        }
+
+
+        if(i<row && j< column)
+        {
+            path(path+"V",row,column,i,j+1);
+            path(path+"D",row,column,i+1,j+1);
+            path(path+"H",row,column,i+1,j);
+        }
+        else if(i==row && j<column)
+        {
+            path(path+"V",row,column,i,j+1);
+        }
+        else if(i<row & j==column)
+        {
+            path(path+"H",row,column,i+1,j);
+        }
+
 
 
     }
     public static void main(String[] args) {
 
         System.out.println("There are: "+reachTarget(2,2,0,0)+" ways to reach target");
+        System.out.println("Paths to get to the target: ");
+        path("",2,2,0,0);
     }
 
 }
---
+++ Backtracking/src/MazeObstacles.java
@@ -0,0 +1,2 @@
+package Backtracking.src;public class MazeObstacles {
+}
