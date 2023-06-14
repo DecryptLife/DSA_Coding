1a968a8af932c9fdb077fbc9049f9631bda985f7
DecryptLife <benson.thomas2602@gmail.com>
2023-06-13 23:26:28 -0500

    Maze with Obstacles

--- Backtracking/src/MazeObstacles.java
+++ Backtracking/src/MazeObstacles.java
@@ -1,2 +1,79 @@
-package Backtracking.src;public class MazeObstacles {
+package Backtracking.src;
+
+public class MazeObstacles {
+
+    private final static int[] obstacle = {1,1};
+    public static int reachTarget(int row, int column, int i, int j)
+    {
+
+        if(i == obstacle[0] && j== obstacle[1])
+            return 0;
+
+        if(row == i && column ==j)
+        {
+            return 1;
+        }
+
+
+
+
+        int right = 0;
+        int down = 0;
+        int diagonal = 0;
+        if(i<row && j< column)
+        {
+            right += reachTarget(row,column,i,j+1);
+            diagonal += reachTarget(row,column,i+1,j+1);
+            down += reachTarget(row,column,i+1,j);
+        }
+        else if(i==row && j<column)
+        {
+            down += reachTarget(row,column,i,j+1);
+        }
+        else if(i<row & j==column)
+        {
+            right += reachTarget(row,column,i+1,j);
+        }
+
+        return right + down +diagonal;
+
+
+    }
+    public static void  path(String path,int row, int column, int i, int j)
+    {
+        if(row == i && column ==j)
+        {
+            System.out.println(path);
+            return;
+        }
+
+        if(i == obstacle[0] && j== obstacle[1])
+            return;
+
+        if(i<row && j< column)
+        {
+            path(path+"V",row,column,i,j+1);
+            path(path+"D",row,column,i+1,j+1);
+            path(path+"H",row,column,i+1,j);
+        }
+        else if(i==row && j<column)
+        {
+            path(path+"V",row,column,i,j+1);
+        }
+        else if(i<row & j==column)
+        {
+            path(path+"H",row,column,i+1,j);
+        }
+
+
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("There are: "+reachTarget(2,2,0,0)+" ways to reach target");
+        System.out.println("Paths to get to the target: ");
+        path("",2,2,0,0);
+    }
+
+
 }
