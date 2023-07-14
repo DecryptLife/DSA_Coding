845bbf65a5e481537f370312e18eb2196ad831c5
DecryptLife <benson.thomas2602@gmail.com>
2023-07-13 23:24:25 -0500

    Optimized Fibonacci

--- Dynamic Programming/src/fib_optimized.java
+++ Dynamic Programming/src/fib_optimized.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class fib_optimized {
+public class fib_optimized {
+
+    public static int fib(int n) {
+
+        int prev2 = 0;
+        int prev = 1;
+        int curr = 0;
+
+        if(n == 0)
+            return prev2;
+
+        if(n == 1)
+            return prev;
+
+        for(int i=2;i<=n;i++){
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return curr;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        int n= 7;
+
+        System.out.println("Fibonnaci number at the index: "+n+" is "+ fib(n));
+    }
 }
