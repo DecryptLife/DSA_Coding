a12d0f6dcfa586e8992de1627025be0cec0859a4
Benson <benson.thomas2602@gmail.com>
2023-09-05 15:25:48 -0500

    Palindrome Partitioning

--- striver/src/palindromePart.java
+++ striver/src/palindromePart.java
@@ -1,2 +1,48 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class palindromePart {
+
+    public static List<List<String>> partitions(String s){
+
+        List<List<String>> res = new ArrayList<>();
+
+        helper(s, new ArrayList<String>(), res, 0);
+
+        return res;
+    }
+
+    public static  void helper(String s, List<String> inner, List<List<String>> outer, int index){
+
+        if(index == s.length()){
+            outer.add(new ArrayList<>(inner));
+            return;
+        }
+
+        for(int i=index;i<s.length();++i){
+
+            if(isPalindrome(s, index, i)) {
+                inner.add(s.substring(index,i+1));
+                helper(s, inner,outer, index+1);
+                inner.remove(inner.size()-1);
+            }
+
+        }
+    }
+
+    public static boolean isPalindrome(String s, int left, int right ){
+
+        while(left <= right){
+            if(s.charAt(left++) != s.charAt(right--))
+                return false;
+        }
+
+        return true;
+
+    }
+    public static void main(String[] args) {
+        String s = "aabb";
+        System.out.println(partitions(s));
+    }
 }
