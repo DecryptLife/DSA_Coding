b12f4dec3af23e2b8fe4589f8a21af3ba8da1638
Benson <benson.thomas2602@gmail.com>
2023-09-13 13:50:55 -0500

    shorter code

--- striver/src/graphs/numProvinces.java
+++ striver/src/graphs/numProvinces.java
@@ -24,16 +24,14 @@ public class numProvinces {
 
         int count = 0;
 
-
         for(int i=1;i<adj.size();i++){
 
             if(!visited[i])
-            {
-                for(Integer j: adj.get(i)){
-                    if(!visited[j])
-                        visited[j] =  true;
-                }
                 count++;
+
+            for(Integer j: adj.get(i)){
+                if(!visited[j])
+                    visited[j] =  true;
             }
 
         }
@@ -46,13 +44,18 @@ public class numProvinces {
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for(int i=0;i<=4;i++){
+        for(int i=0;i<=8;i++){
             adj.add(new ArrayList<>());
         }
-        addEdge(adj,1,3);
-        addEdge(adj,2,4);
+        addEdge(adj,1,2);
+        addEdge(adj,2,3);
 
-//        print(adj);
+        addEdge(adj, 4,5);
+        addEdge(adj, 5,6);
+
+        addEdge(adj,7,8);
+
+        print(adj);
 
         findNumProv(adj);
 
