374ba808504adf25d4b3b3afe9eefc9968307b42
Benson <benson.thomas2602@gmail.com>
2023-09-22 00:27:07 -0500

    Nearest Zeros - better runtime

--- striver/src/graphs/nearestZero.java
+++ striver/src/graphs/nearestZero.java
@@ -18,28 +18,23 @@ public class nearestZero {
         }
     }
     public static int[][] nearest(int[][] matrix){
-        int[][] res = new int[matrix.length][matrix[0].length];
         int m = matrix.length;
         int n = matrix[0].length;
 
+        int[][] res = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+
+        Queue<Pair> queue = new LinkedList<>();
+
         for (int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j] == 0)
-                    res[i][j] = 0;
-                else {
-                    res[i][j] = matrix[i][j];
-                    getNearest(matrix, res, i, j);
+                if(matrix[i][j] == 0) {
+                    queue.add(new Pair(i,j,0));
+                    visited[i][j] = true;
+                    res[i][j]=0;
                 }
             }
         }
-
-        return res;
-    }
-
-    public static void getNearest(int[][] matrix, int[][] res, int row, int col){
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(row, col, 1));
-
 
         int drow[] = {1,0,-1,0};
         int dcol[] = {0,1,0,-1};
@@ -47,30 +42,31 @@ public class nearestZero {
         while(!queue.isEmpty()){
             int r = queue.peek().row;
             int c = queue.peek().col;
-            int step = queue.peek().step;
+            int step =  queue.peek().step;
+            res[r][c] = step;
             queue.remove();
 
             for(int i=0;i<4;i++){
                 int nr = r + drow[i];
                 int nc = c + dcol[i];
 
-               if(nr >=0 && nc >=0 && nr < matrix.length && nc < matrix[0].length){
-                   if(matrix[nr][nc] == 0) {
-                       res[row][col] = step;
-                       return;
-                   }
-                   else {
-                       queue.add(new Pair(nr,nc,step+1));
-                   }
-               }
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && !visited[nr][nc]){
+                    queue.add(new Pair(nr,nc, step+1));
+                    visited[nr][nc] = true;
+                }
             }
+
         }
+
+        return res;
     }
+
+
     public static void main(String[] args) {
         int[][] matrix ={
                 {0,0,0},
                 {0,1,0},
-                {0,0,0}
+                {1,1,1}
         };
 
         int[][] res = nearest(matrix);
