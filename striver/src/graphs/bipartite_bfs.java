9285661206e65e24b8889b0757fcf5f367776a26
DecryptLife <benson.thomas2602@gmail.com>
2023-10-08 23:12:03 -0500

    bipartite graph bfs

--- striver/src/graphs/bipartite_bfs.java
+++ striver/src/graphs/bipartite_bfs.java
@@ -12,12 +12,42 @@ class bipartite_bfs {
   }
 
 
-  public static boolean  isBipartite(ArrayList<ArrayList<Integer>> adjList){
+  public static boolean  isBipartite(ArrayList<ArrayList<Integer>> adjList, int V){
 
-    Queue queue = new LinkedList<>();
-    boolean[] visited = new boolean[adjList.size()]
+    int[] color = new int[V];
+
+    for(int i=0;i<V;i++) color[i] = -1;
+
+    for(int i=0;i<V;i++){
+      if(color[i] == -1){
+        if(!check(adjList, i, V, color)) return false;
+      }
+    }
 
     return true;
+    
+  }
+
+  public static boolean check(ArrayList<ArrayList<Integer>> adjList, int start, int V, int[] color){
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(start);
+    color[start] = 0;
+
+    while(!queue.isEmpty()){
+      int num = queue.peek();
+      queue.remove();
+      for(int ele: adjList.get(num)){
+
+        if(color[ele] == -1){
+          color[ele] = 1 - color[num];
+          queue.add(ele);
+        }
+        else if (color[ele] == color[num]) return false;
+      }
+    }
+    return true;
+
+
   }
   public static void main(String[] args) {
     ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -28,11 +58,15 @@ class bipartite_bfs {
 
     addEdge(adj, 1,2);
     addEdge(adj,2,3);
-    addEdge(adj,2,6);
+    addEdge(adj,2,5);
     addEdge(adj,3,4);
-    addEdge(adj,4,5);
-    addEdge(adj,4,7);
-    addEdge(adj,5,6);
-    addEdge(adj, 7,8);
+    addEdge(adj,5,4);
+    addEdge(adj,4,6);;
+
+    if(isBipartite(adj, adj.size())){
+      System.out.println("Bipartite graph");
+    }
+    else
+      System.out.println("Not bipartite");
   }
 }
