6889be8cb6998789154a5d988de9dd8e8b831395
DecryptLife <benson.thomas2602@gmail.com>
2023-11-06 23:40:58 -0600

    Valid Anagrams

---
+++ NeetCode/Arrays & Hashing/anagram.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+
+
+public class anagram {
+        public boolean isAnagram(String s, String t) {
+    
+    
+            if(s.length() != t.length())
+                return false;
+    
+            char[] sch = s.toCharArray();
+            char[] tch = t.toCharArray();
+    
+            Arrays.sort(sch);
+            Arrays.sort(tch);
+    
+            for(int i=0;i<s.length();i++){
+                if(sch[i] != tch[i])
+                    return false;
+            }
+    
+            return true;
+        }
+}
