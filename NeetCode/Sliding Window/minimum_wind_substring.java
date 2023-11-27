07087e9d9dbce40d96954c3cf2578ba4821574bd
DecryptLife <benson.thomas2602@gmail.com>
2023-11-26 21:51:53 -0600

    optimal soln

--- NeetCode/Sliding Window/minimum_wind_substring.java
+++ NeetCode/Sliding Window/minimum_wind_substring.java
@@ -1,53 +1,49 @@
-class minimum_window_substring {
+class Solution {
 
-    public boolean check(Map<Character, Integer> mpt, Map<Character, Integer> mps ){
-
-        for(Map.Entry<Character, Integer> entry: mpt.entrySet()){
-            if(!mps.containsKey(entry.getKey()) || mps.get(entry.getKey()) < entry.getValue()){
-                return false;
-            }
-        }
-
-        return true;
-    }
+   
     public String minWindow(String s, String t) {
 
         Map<Character, Integer> mpt = new HashMap<>();
 
         Map<Character, Integer> mps = new HashMap<>();
 
-       for(char ch: t.toCharArray()){
+       for(char ch:t.toCharArray()){
            mpt.put(ch, mpt.getOrDefault(ch,0)+1);
        }
 
-       int i=0;int j=0;
+       int i =0; int j=0;
+       int have = 0; int need = mpt.size();
+       int min = Integer.MAX_VALUE;
        String str = "";
-       int ans = Integer.MAX_VALUE;
-
        while(j<s.length()){
            char ch = s.charAt(j);
 
            if(mpt.containsKey(ch)){
-               if(mps.isEmpty()){
-                   i=j;
-               }
                mps.put(ch, mps.getOrDefault(ch,0)+1);
+
+               if(mps.get(ch).equals(mpt.get(ch))){ 
+                   have++;
+                }
            }
 
-           if(j-i+1 >= t.length()){
-               if(check(mpt, mps)){
-                   if(ans > j- i + 1){
-                       str = s.substring(i,j+1);
-                       ans = j-i+1;
-                   }
-                   mps.clear();
-                   i = i+1;
-                   i = j;
-                   continue;
+           while(have == need){
+               char left = s.charAt(i);
+               if(j-i+1 < min){
+                    min = j-i +1;
+                    str = s.substring(i,j+1); 
                }
+               if(mpt.containsKey(left)){
+
+                   mps.put(left, mps.get(left)-1);
+                   if(mps.get(left) < mpt.get(left)){ 
+                         have--;
+                    }
+               }
+               i++;
            }
 
            j++;
+
        }
 
        return str;
