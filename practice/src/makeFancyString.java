0b743ad9334aa7bb70dc1ef1ee60966918746202
DecryptLife <benson.thomas2602@gmail.com>
2024-01-04 23:42:47 -0600

    optimal

--- practice/src/makeFancyString.java
+++ practice/src/makeFancyString.java
@@ -1,24 +1,20 @@
 class Solution {
     public String makeFancyString(String s) {
-        int count = 1;
-        char curr = s.charAt(0);
-        // Stack<Character> stack = new Stack<Character>();
-        String result = ""+s.charAt(0);
-
-        for(int i =1 ;i<s.length();i++){
-            char ch = s.charAt(i);
-
-            if(ch == curr){
-                count++;
-                if(count < 3) result += ch;
+        char prev = s.charAt (0);
+        int freq = 1;
+        StringBuilder res = new StringBuilder ();
+        res.append (s.charAt (0));
+        for (int i = 1; i < s.length (); i++) {
+            if (s.charAt (i) == prev)
+                freq++;
+            else {
+                prev = s.charAt (i);
+                freq = 1;
             }
-            else{
-                count = 1;
-                curr = ch;
-                result += ch;
-            }
+            if (freq < 3)
+                res.append (s.charAt (i));
         }
-        return result;
+        return res.toString ();
         
     }
 }
