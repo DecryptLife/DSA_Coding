38c154819026f8a38444e2c91bc8a12c243571dc
DecryptLife <benson.thomas2602@gmail.com>
2024-01-04 23:41:12 -0600

    better solution

--- practice/src/makeFancyString.java
+++ practice/src/makeFancyString.java
@@ -1,11 +1,11 @@
 class Solution {
     public String makeFancyString(String s) {
-        int count = 0;
-        char curr = ' ';
+        int count = 1;
+        char curr = s.charAt(0);
         // Stack<Character> stack = new Stack<Character>();
-        String result = "";
+        String result = ""+s.charAt(0);
 
-        for(int i =0 ;i<s.length();i++){
+        for(int i =1 ;i<s.length();i++){
             char ch = s.charAt(i);
 
             if(ch == curr){
