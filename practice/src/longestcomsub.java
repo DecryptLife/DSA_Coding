ca12c317f908762cb89c873543d3bf6378000b5f
DecryptLife <benson.thomas2602@gmail.com>
2024-01-26 00:12:12 -0600

    LC.1143 LCS - dp memoization

--- practice/src/longestcomsub.java
+++ practice/src/longestcomsub.java
@@ -3,18 +3,26 @@ class Solution {
         
         int n = text1.length();
         int m = text2.length();
+        int[][] dp = new int[n][m];
 
-        return subs(n-1,m-1, text1, text2);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j] = -1;
+            }
+        }
+        return subs(n-1,m-1, text1, text2, dp);
     }
 
-    public int subs(int i, int j, String s1, String s2){
+    public int subs(int i, int j, String s1, String s2, int[][] dp){
 
         if(i<0 || j<0) return 0;
 
+        if(dp[i][j] != -1) return dp[i][j];
+
         if(s1.charAt(i) == s2.charAt(j)){
-            return 1 + subs(i-1,j-1, s1, s2);
+            return dp[i][j] = 1 + subs(i-1,j-1, s1, s2, dp);
         }
 
-        return Math.max(subs(i-1,j, s1, s2), subs(i,j-1, s1, s2));
+        return dp[i][j] = Math.max(subs(i-1,j, s1, s2, dp), subs(i,j-1, s1, s2, dp));
     }
 }
